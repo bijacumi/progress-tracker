6fec3e484bfea369164e32c4cf223b27a2be808a
Mihai Bijacu <mihaibijacu@Mihais-MacBook-Pro-2.local>
2025-04-29 14:52:18 -0400

    Fix API URL double slash issue

--- Front-End/src/environments/environment.prod.ts
+++ Front-End/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://progresstracker-production.up.railway.app/',
+  apiUrl: 'https://progresstracker-production.up.railway.app',
 };
